821044f7ce276853b95d561916751451c9b92052
fscottfoti <fscottfoti@gmail.com>
2024-04-21 14:29:54 -0700

    docstrings and typing

--- pandana2/aggregations.py
+++ pandana2/aggregations.py
@@ -1,40 +1,48 @@
 import geopandas as gpd
 import pandas as pd
-from typing import Callable
-
-
-def nearest_nodes(values_gdf: gpd.GeoDataFrame, nodes_gdf: gpd.GeoDataFrame):
-    """
-    Map each point in values_gdf to its nearest node in nodes_gdf
-    :param values_gdf:
-    :param nodes_gdf:
-    :return:
-    """
-    return (
-        values_gdf.to_crs(epsg=3857)
-        .sjoin_nearest(nodes_gdf.to_crs(epsg=3857))
-        .rename(columns={"index_right": "node_id"})
-        .set_index("node_id")
-    )
-
-
-def linear_decay_aggregation(
-    max_distance: float, value_col: str, aggregation: str, weight_col="weight"
-):
-    return (
-        lambda x: (
-            x[value_col] * (max_distance - x[weight_col]).clip(lower=0) / max_distance
-        )
-        .agg(aggregation)
-        .round(3)
-    )
+from typing import Callable, Union
 
 
 def no_decay_aggregation(
-    max_distance: float, value_col: str, aggregation: str, weight_col="weight"
+    max_weight: float, value_col: str, aggregation: str, weight_col="weight"
 ):
+    """
+    Network aggregations with no decay.  Values will be filtered out where weight_col
+        is greater than max_weight, then value_col is aggregated using the passed aggregation
+        method.  No decay means a value at max_weight will be weighted equally to a value
+        at the origin node.
+    :param max_weight: Values beyond max_weight (sum of weight_col in network distance)
+        will not be considered
+    :param value_col: The column in values_df to aggregate
+    :param aggregation: The aggregation to use, can be anything which pandas.DataFrame.agg accepts
+    :param weight_col: The column in edges_df to use as the weight column
+    :return: A value for the given origin node
+    """
+    return lambda x: x[x[weight_col] <= max_weight][value_col].agg(aggregation).round(3)
+
+
+def linear_decay_aggregation(
+    max_weight: float, value_col: str, aggregation: str, weight_col="weight"
+):
+    """
+    Network aggregations with linear decay.  Values will be filtered out where weight_col
+        is greater than max_weight, then value_col is aggregated using the passed aggregation
+        method.  Linear decay means a value at max_weight will be weighted as zero while a value
+        at the origin node is weighted at 1 and a weight halfway to max_weight will be weighted
+        as 0.5.
+    :param max_weight: Values beyond max_weight (sum of weight_col in network distance)
+        will not be considered
+    :param value_col: The column in values_df to aggregate
+    :param aggregation: The aggregation to use, can be anything which pandas.DataFrame.agg accepts
+    :param weight_col: The column in edges_df to use as the weight column
+    :return: A value for the given origin node
+    """
     return (
-        lambda x: x[x[weight_col] <= max_distance][value_col].agg(aggregation).round(3)
+        lambda x: (
+            x[value_col] * (max_weight - x[weight_col]).clip(lower=0) / max_weight
+        )
+        .agg(aggregation)
+        .round(3)
     )
 
 
@@ -42,9 +50,26 @@ def aggregate(
     values_df: pd.DataFrame,
     edges_df: pd.DataFrame,
     group_func: Callable[[pd.DataFrame], float],
-) -> pd.DataFrame:
+    origin_node_id_col: str = "from",
+    destination_node_id_col: str = "to",
+) -> Union[pd.Series, pd.DataFrame]:
+    """
+    Given a values_df which is indexed by node_id and an edges_df with a weight column,
+        merge the edges_df to values_df using the destination node id, group by the
+        origin node_id, and perform the aggregation specified by group_func
+    :param values_df: Typically returned by pandana2.nearest_nodes
+    :param edges_df: Typically returned by pandana2.make_edges
+    :param group_func: Typically one of the aggregation functions in this module, e.g.
+        linear_decay_aggregation, but can be customized
+    :param destination_node_id_col: a column in edges_df, usually 'to'
+    :param origin_node_id_col: a column in edges_df, usually 'from'
+    :return: A series indexes by all the origin node ids in edges_df with values returned
+        by group_func
+    """
     return (
-        edges_df.merge(values_df, how="inner", left_on="to", right_index=True)
-        .groupby("from")
+        edges_df.merge(
+            values_df, how="inner", left_on=destination_node_id_col, right_index=True
+        )
+        .groupby(origin_node_id_col)
         .apply(group_func, include_groups=False)
     )
--- pandana2/network.py
+++ pandana2/network.py
@@ -32,7 +32,38 @@ def make_edges(
     )
 
 
-def make_nodes(graph: nx.Graph, x_col="x", y_col="y", crs=4326) -> gpd.GeoDataFrame:
+def nearest_nodes(
+    values_gdf: gpd.GeoDataFrame, nodes_gdf: gpd.GeoDataFrame
+) -> gpd.GeoDataFrame:
+    """
+    Map each point in values_gdf to its nearest node in nodes_gdf
+    :param values_gdf: A GeoDataFrame (usually points) with columns for values (e.g. a
+        GeoDataFrame of amenity locations, or population or jobs
+    :param nodes_gdf: The return value of pandana2.make_nodes
+    :return: A GeoDataFrame similar to values_gdf which now contains a node_id column, suitable
+        to pass to pandana2.aggregate
+    """
+    return (
+        values_gdf.to_crs(epsg=3857)
+        .sjoin_nearest(nodes_gdf.to_crs(epsg=3857))
+        .rename(columns={"index_right": "node_id"})
+        .set_index("node_id")
+    )
+
+
+def make_nodes(
+    graph: nx.Graph, x_col: str = "x", y_col: str = "y", crs: int = 4326
+) -> gpd.GeoDataFrame:
+    """
+    Given a graph, return a GeoDataFrame of point geometry where the index is node_ids.  This
+        is used by nearest nodes (to map a variable to the network), but isn't strictly required
+
+    :param graph: a networkx graph, e.g. from osmnx.graph_from_place("Oakland, CA", network_type="walk")
+    :param x_col: attribute of each node which contains the x value (osmnx uses 'x')
+    :param y_col:  attribute of each node which contains the y value (osmnx uses 'y')
+    :param crs: the coordinate reference system of the GeoDataFrame (default is lat-lng)
+    :return:
+    """
     return gpd.GeoDataFrame(
         geometry=[
             shapely.Point(graph.nodes[node][x_col], graph.nodes[node][y_col])
