ba89135c16c49899f0ebfa0ae2873f763e122251
fscottfoti <fscottfoti@gmail.com>
2024-04-23 11:49:02 -0700

    more

--- pandana2/__init__.py
+++ pandana2/__init__.py
@@ -1,2 +1,3 @@
 from pandana2.aggregations import *
+from pandana2.dijkstra import *
 from pandana2.network import *
--- pandana2/dijkstra.py
+++ pandana2/dijkstra.py
@@ -52,31 +52,3 @@ def dijkstra(
                 heappush(q, (new_weight, to_node))
 
     return min_weights
-
-
-if __name__ == "__main__":
-    edges = pd.DataFrame(
-        [
-            (1, 2, 7),
-            (1, 4, 5),
-            (2, 3, 8),
-            (2, 4, 9),
-            (2, 5, 7),
-            (3, 5, 5),
-            (4, 5, 15),
-            (4, 6, 6),
-            (5, 6, 8),
-            (5, 7, 9),
-            (6, 7, 11),
-        ],
-        columns=["from", "to", "weight"],
-    )
-    edges["weight"] = edges["weight"].astype("float64")
-
-    print(edges)
-    assert dijkstra(
-        edges["from"].values, edges["to"].values, edges.weight.values, 1, 15
-    ) == {1: 0, 2: 7, 4: 5, 5: 14, 6: 11, 3: 15, 7: 22}
-    assert dijkstra(
-        edges["from"].values, edges["to"].values, edges.weight.values, 6, 15
-    ) == {6: 0, 7: 11}
