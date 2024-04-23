c2cb5b27d2c1d03e2fa88a0a7a5798df459487e4
Fletcher Foti <fscottfoti@gmail.com>
2024-04-23 12:47:26 -0700

    Dijkstra implementation using numba (#6)
    * python dijkstra with heap is working
    
    * closer
    
    * working I think
    
    * nodes as ints
    
    * working using numba
    
    * more
    
    * implement cutoff correctly
    
    * weight -> cost
    
    * add numba
    
    * refer to early code

---
+++ pandana2/dijkstra.py
@@ -0,0 +1,57 @@
+from heapq import heappop, heappush
+import numba
+from numba.types import int64, float64, DictType
+import numpy as np
+
+# early code (heavily modified) from https://gist.github.com/kachayev/5990802
+
+
+@numba.jit((DictType(int64, float64))(int64[:], int64[:], float64[:], int64, float64))
+def dijkstra(
+    from_nodes: np.array,  # node ids (ints)
+    to_nodes: np.array,  # node ids (ints)
+    edge_costs: np.array,  # weights (floats)
+    source: int,  # source node (str for now, will be int)
+    cutoff: float,  # cutoff weight (float)
+):
+    assert (
+        len(from_nodes) == len(to_nodes) == len(edge_costs)
+    ), "from_nodes, to_nodes, and edge_weights must be same length"
+    indexes = {}
+    for i in range(len(from_nodes)):
+        if i > 1:
+            # we require from_nodes to be sorted
+            assert from_nodes[i] >= from_nodes[i - 1], "from_nodes must be sorted"
+        if from_nodes[i] not in indexes:
+            # we assume from_nodes is sorted
+            # indexes[node_id] holds the first array index that from_node is seen in from_nodes
+            indexes[from_nodes[i]] = i
+
+    # q is the heapq instance
+    # seen is a set of which nodes we've seen so far
+    # min_weight is a dict where keys are node ids and values are the minimum weights we've seen so far
+    q, seen, min_costs = [(0.0, source)], set(), {source: 0.0}
+    while q:
+        (current_cost, from_node) = heappop(q)
+        if from_node in seen or from_node not in indexes:
+            continue
+
+        seen.add(from_node)
+        ind = indexes[from_node]
+
+        while ind < len(from_nodes) and from_nodes[ind] == from_node:
+            to_node, cost = to_nodes[ind], edge_costs[ind]
+            ind += 1
+            if to_node in seen:
+                continue
+
+            prev_cost = min_costs.get(to_node)
+            new_cost = current_cost + cost
+            if new_cost > cutoff:
+                continue
+
+            if prev_cost is None or new_cost < prev_cost:
+                min_costs[to_node] = new_cost
+                heappush(q, (new_cost, to_node))
+
+    return min_costs
---
+++ tests/test_dijkstra.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from pandana2 import dijkstra
+
+
+def test_dijkstra_basic():
+    edges = pd.DataFrame(
+        [
+            (1, 2, 7),
+            (1, 4, 5),
+            (2, 3, 8),
+            (2, 4, 9),
+            (2, 5, 7),
+            (3, 5, 5),
+            (4, 5, 15),
+            (4, 6, 6),
+            (5, 6, 8),
+            (5, 7, 9),
+            (6, 7, 11),
+        ],
+        columns=["from", "to", "weight"],
+    )
+    edges["weight"] = edges["weight"].astype("float64")
+
+    assert dict(
+        dijkstra(edges["from"].values, edges["to"].values, edges.weight.values, 1, 15)
+    ) == {1: 0, 2: 7, 4: 5, 5: 14, 6: 11, 3: 15}
+    assert dict(
+        dijkstra(edges["from"].values, edges["to"].values, edges.weight.values, 6, 15)
+    ) == {6: 0, 7: 11}
