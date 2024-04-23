5f484c133bf82ec1a0ae012bf21efab63fd35610
fscottfoti <fscottfoti@gmail.com>
2024-04-23 11:15:08 -0700

    nodes as ints

--- pandana2/dijkstra.py
+++ pandana2/dijkstra.py
@@ -7,7 +7,7 @@ def dijkstra(
     from_nodes: np.array,  # node ids (ints)
     to_nodes: np.array,  # node ids (ints)
     edge_weights: np.array,  # weights (floats)
-    source: str,  # source node (str for now, will be int)
+    source: int,  # source node (str for now, will be int)
     cutoff: int,  # cutoff weight (float)
 ):
     indexes = {}
@@ -54,25 +54,25 @@ def dijkstra(
 if __name__ == "__main__":
     edges = pd.DataFrame(
         [
-            ("A", "B", 7),
-            ("A", "D", 5),
-            ("B", "C", 8),
-            ("B", "D", 9),
-            ("B", "E", 7),
-            ("C", "E", 5),
-            ("D", "E", 15),
-            ("D", "F", 6),
-            ("E", "F", 8),
-            ("E", "G", 9),
-            ("F", "G", 11),
+            (1, 2, 7),
+            (1, 4, 5),
+            (2, 3, 8),
+            (2, 4, 9),
+            (2, 5, 7),
+            (3, 5, 5),
+            (4, 5, 15),
+            (4, 6, 6),
+            (5, 6, 8),
+            (5, 7, 9),
+            (6, 7, 11),
         ],
         columns=["from", "to", "weight"],
     )
 
     print(edges)
     assert dijkstra(
-        edges["from"].values, edges["to"].values, edges.weight.values, "A", 15
-    ) == {"A": 0, "B": 7, "D": 5, "E": 14, "F": 11, "C": 15, "G": 22}
+        edges["from"].values, edges["to"].values, edges.weight.values, 1, 15
+    ) == {1: 0, 2: 7, 4: 5, 5: 14, 6: 11, 3: 15, 7: 22}
     assert dijkstra(
-        edges["from"].values, edges["to"].values, edges.weight.values, "F", 15
-    ) == {"F": 0, "G": 11}
+        edges["from"].values, edges["to"].values, edges.weight.values, 6, 15
+    ) == {6: 0, 7: 11}
