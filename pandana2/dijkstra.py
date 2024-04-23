ecf0bc8037c3326952a53b707e5894f94b81764b
fscottfoti <fscottfoti@gmail.com>
2024-04-23 11:09:38 -0700

    closer

--- pandana2/dijkstra.py
+++ pandana2/dijkstra.py
@@ -4,29 +4,55 @@ import numpy as np
 import pandas as pd
 
 
-def dijkstra(edge_from: np.array, edge_to: np.array, edge_weight: np.array, f, t):
-    g = defaultdict(list)
-    for i in range(edge_from.size):
-        g[edge_from[i]].append((edge_weight[i], edge_to[i]))
+def dijkstra(
+    from_nodes: np.array,  # node ids (ints)
+    to_nodes: np.array,  # node ids (ints)
+    edge_weights: np.array,  # weights (floats)
+    source: str,  # source node (str for now, will be int)
+    cutoff: int,  # cutoff weight (float)
+):
+    indexes = {}
+    for i in range(len(from_nodes)):
+        if i > 1:
+            # we require from_nodes to be sorted
+            assert from_nodes[i] >= from_nodes[i - 1]
+        if from_nodes[i] not in indexes:
+            # we assume from_nodes is sorted
+            # indexes[node_id] holds the first array index that from_node is seen in from_nodes
+            indexes[from_nodes[i]] = i
 
-    q, seen, min_weight = [(0, f)], set(), {f: 0}
+    # q is the heapq instance
+    # seen is a set of which nodes we've seen so far
+    # min_weight is a dict where keys are node ids and values are the minimum weights we've seen so far
+    q, seen, min_weights = [(0, source)], set(), {source: 0}
     while q:
-        (cost, v1) = heappop(q)
-        if v1 not in seen:
-            seen.add(v1)
-            if v1 == t:
-                return cost
+        print(q)
+        (current_cost, from_node) = heappop(q)
+        print(from_node, current_cost)
+        if from_node in seen:
+            continue
 
-            for c, v2 in g.get(v1, ()):
-                if v2 in seen:
-                    continue
-                prev_weight = min_weight.get(v2, None)
-                next_weight = cost + c
-                if prev_weight is None or next_weight < prev_weight:
-                    min_weight[v2] = next_weight
-                    heappush(q, (next_weight, v2))
+        seen.add(from_node)
+        if current_cost >= cutoff:
+            return min_weights
 
-    return float("inf"), None
+        if from_node not in indexes:
+            continue
+
+        ind = indexes[from_node]
+        while ind < len(from_nodes) and from_nodes[ind] == from_node:
+            to_node, cost = to_nodes[ind], edge_weights[ind]
+            ind += 1
+            print("  ", from_node, to_node, cost)
+            if to_node in seen:
+                continue
+            prev_weight = min_weights.get(to_node, None)
+            new_weight = current_cost + cost
+            if prev_weight is None or new_weight < prev_weight:
+                min_weights[to_node] = new_weight
+                heappush(q, (new_weight, to_node))
+
+    return min_weights
 
 
 if __name__ == "__main__":
@@ -47,17 +73,10 @@ if __name__ == "__main__":
         columns=["from", "to", "weight"],
     )
 
-    print("=== Dijkstra ===")
     print(edges)
-    assert (
-        dijkstra(
-            edges["from"].values, edges["to"].values, edges.weight.values, "A", "E"
-        )
-        == 14
-    )
-    assert (
-        dijkstra(
-            edges["from"].values, edges["to"].values, edges.weight.values, "F", "G"
-        )
-        == 11
-    )
+    assert dijkstra(
+        edges["from"].values, edges["to"].values, edges.weight.values, "A", 15
+    ) == {"A": 0, "B": 7, "D": 5, "E": 14, "F": 11, "C": 15, "G": 22}
+    assert dijkstra(
+        edges["from"].values, edges["to"].values, edges.weight.values, "F", 15
+    ) == {"F": 0, "G": 11}
