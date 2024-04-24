6b8632e8eb66ff80e3e9f9d7fba3afb508e9a6dc
fscottfoti <fscottfoti@gmail.com>
2024-04-24 16:54:26 -0700

    kind of working again maybe

--- pandana2/aggregations.py
+++ pandana2/aggregations.py
@@ -1,6 +1,10 @@
-import geopandas as gpd
+import numba
+from numba.typed import Dict, List
+from numba.types import float64, int64, ListType, DictType
+import numpy as np
 import pandas as pd
 from typing import Callable, Union
+from pandana2.dijkstra import dijkstra
 
 
 def no_decay_aggregation(
@@ -22,7 +26,7 @@ def no_decay_aggregation(
 
 
 def linear_decay_aggregation(
-    max_weight: float, value_col: str, aggregation: str, weight_col="weight"
+    aggregation_func=np.sum,
 ):
     """
     Network aggregations with linear decay.  Values will be filtered out where weight_col
@@ -32,26 +36,19 @@ def linear_decay_aggregation(
         as 0.5.
     :param max_weight: Values beyond max_weight (sum of weight_col in network distance)
         will not be considered
-    :param value_col: The column in values_df to aggregate
-    :param aggregation: The aggregation to use, can be anything which pandas.DataFrame.agg accepts
-    :param weight_col: The column in edges_df to use as the weight column
+    :param aggregation_func: The aggregation to use, can be anything which pandas.DataFrame.agg accepts
     :return: A value for the given origin node
     """
-    return (
-        lambda x: (
-            x[value_col] * (max_weight - x[weight_col]).clip(lower=0) / max_weight
-        )
-        .agg(aggregation)
-        .round(3)
+    return lambda values, weights, cutoff: (
+        aggregation_func(values * (cutoff - weights).clip(lower=0) / cutoff)
     )
 
 
 def aggregate(
-    values_df: pd.DataFrame,
+    values_df: pd.Series,
+    all_nodes_ids: pd.Index,
     edges_df: pd.DataFrame,
-    group_func: Callable[[pd.DataFrame], float],
-    origin_node_id_col: str = "from",
-    destination_node_id_col: str = "to",
+    cutoff: float,
 ) -> Union[pd.Series, pd.DataFrame]:
     """
     Given a values_df which is indexed by node_id and an edges_df with a weight column,
@@ -66,10 +63,57 @@ def aggregate(
     :return: A series indexes by all the origin node ids in edges_df with values returned
         by group_func
     """
-    return (
-        edges_df.merge(
-            values_df, how="inner", left_on=destination_node_id_col, right_index=True
-        )
-        .groupby(origin_node_id_col)
-        .apply(group_func, include_groups=False)
+    edges_df = edges_df.reset_index()
+
+    values_dict = Dict.empty(key_type=int64, value_type=ListType(float64))
+    for node_id, value in values_df.items():
+        if node_id not in values_dict:
+            values_dict[node_id] = List(lsttype=ListType(float64))
+        values_dict[node_id].append(float(value))
+
+    return pd.Series(
+        _aggregate(
+            all_nodes_ids.values,
+            values_dict,
+            edges_df["u"].values,
+            edges_df["v"].values,
+            edges_df["length"].values,
+            cutoff,
+        ),
+        index=all_nodes_ids,
     )
+
+
+@numba.jit(
+    float64[:](
+        int64[:],
+        DictType(int64, ListType(float64)),
+        int64[:],
+        int64[:],
+        float64[:],
+        float64,
+    )
+)
+def _aggregate(
+    node_ids: np.array,  # all node_ids
+    values_dict: dict,  # keys are node ids and values are a list of values
+    from_nodes: np.array,  # start node id of edges (ints)
+    to_nodes: np.array,  # end node_id of edges (ints)
+    edge_costs: np.array,  # weights (floats)
+    cutoff: float64,
+):
+    ret = np.empty(len(node_ids), dtype="float64")
+    for i in range(len(node_ids)):
+        min_weights = dijkstra(from_nodes, to_nodes, edge_costs, node_ids[i], cutoff)
+
+        values, weights = [], []
+        for node_id, weight in min_weights.items():
+            if node_id in values_dict:
+                for value in values_dict[node_id]:
+                    values.append(value)
+                    weights.append(weight)
+        values, weights = np.array(values), np.array(weights)
+
+        # hardcoded aggregation function
+        ret[i] = np.sum(values * np.clip(cutoff - weights, 0, None) / cutoff)
+    return ret
--- tests/test_pandana2.py
+++ tests/test_pandana2.py
@@ -99,16 +99,18 @@ def get_amenity_as_dataframe(place_query: str, amenity: str):
 
 def test_workflow():
     place_query = "Orinda, CA"
-    g = osmnx.graph_from_place(place_query)
-    edges = pandana2.make_edges(g, 500)
-    nodes = pandana2.make_nodes(g)
+    graph = osmnx.graph_from_place(place_query)
+    nodes, edges = osmnx.graph_to_gdfs(graph)
 
     restaurants_df = get_amenity_as_dataframe(place_query, "restaurant")
     restaurants_df = pandana2.nearest_nodes(restaurants_df, nodes)
     assert restaurants_df.index.isin(nodes.index).all()
 
-    group_func = pandana2.linear_decay_aggregation(500, "count", "sum")
-    aggregations_series = pandana2.aggregate(restaurants_df, edges, group_func)
+    # agg_func = pandana2.linear_decay_aggregation(500, "count", "sum")
+    aggregations_series = pandana2.aggregate(
+        restaurants_df["count"], nodes.index, edges, 500
+    )
+    print(aggregations_series)
     assert aggregations_series.index.isin(nodes.index).all()
     assert aggregations_series.min() >= 0
     assert aggregations_series.max() <= 8
